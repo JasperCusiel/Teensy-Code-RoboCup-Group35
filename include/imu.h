b339a683ac9a9a3595ff8d9e94bc3d5ab5a135c7
Jasper Cusiel <jaspercusiel@icloud.com>
2026-07-20 14:48:40 +1200

    Create IMU module and startup checks.

---
+++ include/imu.h
@@ -0,0 +1,12 @@
+//
+// Created by Jasper Cusiel on 20/07/2026.
+//
+
+#ifndef ROBOCUP_IMU_H
+#define ROBOCUP_IMU_H
+
+#include <stdbool.h>
+bool imu_init();
+void displaySensorDetails(void);
+void displaySensorStatus(void);
+#endif // ROBOCUP_IMU_H
---
+++ src/imu.cpp
@@ -0,0 +1,87 @@
+//
+// Created by Jasper Cusiel on 20/07/2026.
+//
+
+#include <imu.h>
+#include <Adafruit_BNO055.h>
+
+#define FUSION_RUNNING 5
+#define SYSTEM_STARTED 0x0F
+#define NO_ERROR 0
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire1);
+
+void displaySensorStatus(void)
+{
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
+
+  /* Display the results in the Serial Monitor */
+  Serial.println("");
+  Serial.print("System Status: 0x");
+  Serial.println(system_status, HEX);
+  Serial.print("Self Test:     0x");
+  Serial.println(self_test_results, HEX);
+  Serial.print("System Error:  0x");
+  Serial.println(system_error, HEX);
+  Serial.println("");
+  delay(500);
+}
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+bool imu_init() {
+  uint8_t sysStatus, selfTest, sysError;
+
+  if (!bno.begin()) {
+    Serial.println("IMU not started");
+    return false;
+  }
+  bno.setMode(OPERATION_MODE_CONFIG);
+  delay(50);
+  // bno.setExtCrystalUse(true);
+
+  bno.setMode(OPERATION_MODE_NDOF);
+  delay(100);
+
+
+  for (int i = 0; i < 20; i++) {  // 2 seconds max
+    bno.getSystemStatus(&sysStatus, &selfTest, &sysError);
+
+    if (sysStatus == FUSION_RUNNING) break;
+
+    delay(100);
+  }
+  displaySensorDetails();
+  displaySensorStatus();
+  Serial.printf("IMU   SYS: %d | SELF: 0x%02X | ERR: %d\n",
+            sysStatus, selfTest, sysError);
+
+  if (sysStatus != FUSION_RUNNING) {
+    Serial.println("IMU fusion not running yet");
+    return false;
+  }
+
+  if (sysError != NO_ERROR) {
+    Serial.println("IMU system error");
+    return false;
+  }
+
+  return true;
+}
