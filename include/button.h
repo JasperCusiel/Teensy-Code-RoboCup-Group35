048ac4c12b86805cd95c28cb6cd40c25b100b688
Jasper Cusiel <jaspercusiel@icloud.com>
2026-08-05 10:16:43 +1200

    Move into seperate module for reusability

---
+++ include/button.h
@@ -0,0 +1,11 @@
+//
+// Created by Jasper Cusiel on 03/08/2026.
+//
+
+#ifndef ROBOCUP_BUTTIN_H
+#define ROBOCUP_BUTTIN_H
+#include <stdint.h>
+
+bool read_button(uint8_t pin);
+
+#endif // ROBOCUP_BUTTIN_H
---
+++ src/button.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Jasper Cusiel on 03/08/2026.
+//
+#include "button.h"
+#include <arduino.h>
+
+#define DEBOUNCE_TIME_MS 20
+
+bool read_button(uint8_t pin) {
+  static uint32_t lastChange = 0;
+  static bool lastState = HIGH;
+
+  bool current = !digitalRead(pin);
+
+  if (current != lastState) {
+    lastChange = millis();
+    lastState = current;
+  }
+
+  if (millis() - lastChange > DEBOUNCE_TIME_MS) {
+    return current;
+  }
+
+  return lastState;
+}
