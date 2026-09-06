e0163a722587efa6927837d4c2138ddcc79a702b
Jasper Cusiel <jaspercusiel@icloud.com>
2026-09-06 15:58:19 +1200

    Port over from MATLAB code

--- include/frontier-detection.h
+++ include/frontier-detection.h
@@ -5,4 +5,19 @@
 #ifndef ROBOCUP_FRONTIER_DETECTION_H
 #define ROBOCUP_FRONTIER_DETECTION_H
 
+#include <stdint.h>
+
+// A navigable target on the boundary between mapped free space and unmapped space.
+typedef struct {
+  int x;
+  int y;
+  uint16_t size;
+} frontier_goal_t;
+
+// Returns true when this free cell borders at least one unknown cell.
+bool frontier_is_cell(int x, int y);
+
+bool frontier_find_largest_goal(int robot_x, int robot_y,
+                                frontier_goal_t *goal);
+
 #endif // ROBOCUP_FRONTIER_DETECTION_H
