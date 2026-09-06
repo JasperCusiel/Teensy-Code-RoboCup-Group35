ad8d81f5b9ec7b3f477525c54fed498edff120fb
Jasper Cusiel <jaspercusiel@icloud.com>
2026-09-07 10:42:06 +1200

    Tidy and comment.

--- include/colour-sensor.h
+++ include/colour-sensor.h
@@ -6,18 +6,25 @@
 #define ROBOCUP_COLOUR_SENSOR_H
 #include <stdint.h>
 
-typedef enum {COLOR_GREEN, COLOR_BLUE, COLOR_UNDEFINED} base_color_t;
+typedef enum { COLOR_GREEN, COLOR_BLUE, COLOR_UNDEFINED } base_color_t;
 
-typedef struct {
-  uint16_t red;
-  uint16_t green;
-  uint16_t blue;
-  uint16_t clear;
-  base_color_t base_color;
+// Used to store base colour profile
+typedef struct
+{
+    uint16_t red;
+    uint16_t green;
+    uint16_t blue;
+    uint16_t clear;
+    base_color_t base_color;
 } color_t;
 
+// Starts sensor and sets the base color.
 bool colour_sensor_init();
+
+// Gets raw data and determines base color
 void update_base_color();
+
+// Returns stored base color
 base_color_t get_base_color();
 
 #endif // ROBOCUP_COLOUR_SENSOR_H
--- src/colour-sensor.cpp
+++ src/colour-sensor.cpp
@@ -7,34 +7,46 @@
 #define I2C_BUS &Wire1
 #define I2C_ADDRESS 0x29
 
-
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 
+// Stores the color profile of the base
 color_t base = {0, 0, 0, 0, COLOR_UNDEFINED};
 
-bool colour_sensor_init() {
-  for (int i = 0; i < 20; i++) {  // 2 seconds max
-    if (tcs.begin(I2C_ADDRESS, I2C_BUS) == true) {
-      // Get base color
-      update_base_color();
-      return true;
+// Initialize sensor on I2C and set the base color (we assume the robot is always started in the colored home base square.
+bool colour_sensor_init()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        // 2 seconds max
+        if (tcs.begin(I2C_ADDRESS, I2C_BUS) == true)
+        {
+            // Get base color
+            update_base_color();
+            return true;
+        }
+        delay(100);
     }
-    delay(100);
-  }
 
-
-  return false;
+    return false;
 }
 
-void update_base_color() {
-  tcs.getRawData(&base.red, &base.green, &base.blue, &base.clear);
-  if (base.green > base.blue) {
-    base.base_color = COLOR_GREEN;
-  } else {
-    base.base_color = COLOR_BLUE;
-  }
+void update_base_color()
+{
+    // Get raw reading from colour sensor
+    tcs.getRawData(&base.red, &base.green, &base.blue, &base.clear);
+
+    // Basic check to determine base color (blue base has some level of green in it but the green content shows much stronger on the green base)
+    if (base.green > base.blue)
+    {
+        base.base_color = COLOR_GREEN;
+    }
+    else
+    {
+        base.base_color = COLOR_BLUE;
+    }
 }
 
-base_color_t get_base_color() {
-  return base.base_color;
+base_color_t get_base_color()
+{
+    return base.base_color;
 }
