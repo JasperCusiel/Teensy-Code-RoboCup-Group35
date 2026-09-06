a188c958ed9244cd9169624d75e1d8b3f68963c5
Jasper Cusiel <jaspercusiel@icloud.com>
2026-09-06 16:03:20 +1200

    Add init for new modules, add turning led panel on at startup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
-#include "Wire.h"
-#include <Arduino.h>
 #include "ToF-Sensors.h"
+#include "Wire.h"
+#include "autonomy.h"
 #include "display.h"
+#include "drivetrain.h"
+#include "mapping.h"
+#include "mission.h"
+#include "motion-controller.h"
+#include "odometry.h"
+#include "sensors.h"
 #include "smart-servo.h"
-#include <imu.h>
-#include <optical-flow.h>
+#include "telemetry.h"
+#include "weight-detection.h"
+#include <Arduino.h>
+#include <FastLED.h>
 #include <colour-sensor.h>
+#include <imu.h>
 #include <inductive-sensor.h>
 #include <lift-motor.h>
 #include <limit-switch.h>
+#include <optical-flow.h>
 #include <vfh.h>
-#include "odometry.h"
-#include "weight-detection.h"
-#include "sensors.h"
-#include "mapping.h"
 
 #include "scheduler.h"
 #define HZ_TO_US(x) (1000000UL / (x)) // convert hz to micro seconds
@@ -22,21 +28,25 @@
 
 #define GO_BTN A9
 
-#test 
+#define NUM_LEDS 32
+#define DATA_PIN A12
+CRGB leds[NUM_LEDS];
+
 
 uint32_t last_time_1 = micros();
 
 float state[3];
 
 task_t tasks[] = {
+  { imu_get_reading, HZ_TO_US(95), 0 },
   { odometry_update,  HZ_TO_US(95), 0 },
-  { compute_vfh,  HZ_TO_US(10), 0 },
+  { autonomy_motion_task, HZ_TO_US(95), 0 },
   { draw,   HZ_TO_US(5),  0 },
-{ update_input,   HZ_TO_US(5),  0 },
+  { update_input,   HZ_TO_US(5),  0 },
   { get_tof_reading, HZ_TO_US(6),  0 },
-//{ print_ekf_pose, HZ_TO_US(1),  0 },
-{ imu_get_reading, HZ_TO_US(95),  0 },
-//{ mapping_task, HZ_TO_US(6),  0 }
+  { mapping_task, HZ_TO_US(6),  0 },
+  { autonomy_task, HZ_TO_US(20), 0 },
+  { telemetry_map_task, HZ_TO_US(6), 0 }
 };
 
 
@@ -67,6 +77,7 @@ void scanI2C() {
 }
 
 
+
 void setup() {
   Serial.begin(115200);
   Wire.begin();
@@ -75,13 +86,30 @@ void setup() {
   display_init();
   sensors_init();
   vfh_init();
+  colour_sensor_init();
   odometry_init();
   mapping_init();
+  telemetry_init();
+  drivetrain_init();
+  // motion_controller_set_output_callback(set_motor_speeds);
+  // set_motor_speeds(0.9f, 0.9f);
+  autonomy_init();
+  Serial.print("Base Colour: ");
+  if (get_base_color() == COLOR_GREEN) {
+    Serial.println("GREEN");
+  } else {
+    Serial.println("BLUE");
+  }
 
   // scanI2C();
 
   // Calibration
   //get_ToFCalibration();
+
+  FastLED.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
+  FastLED.setBrightness(128);
+  fill_solid(leds, NUM_LEDS, CRGB::White);
+  FastLED.show();
 
   Serial.println("Push GO BTN to start");
   // Wait for GO button to be pushed to start program
@@ -91,9 +119,8 @@ void setup() {
     delay(1);
   }
   Serial.println("Start");
-
-  float target_angle = 0.0f;
-  set_target_angle(target_angle);
+  mission_start();
+  scheduler_init(tasks, NUM_TASKS);
 }
 
 
