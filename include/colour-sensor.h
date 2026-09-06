ea2e03bc3cb80e323681938fa382a9257b4574c1
Jasper Cusiel <jaspercusiel@icloud.com>
2026-09-06 15:50:50 +1200

    Add functions to expose base color identification

--- include/colour-sensor.h
+++ include/colour-sensor.h
@@ -4,7 +4,20 @@
 
 #ifndef ROBOCUP_COLOUR_SENSOR_H
 #define ROBOCUP_COLOUR_SENSOR_H
+#include <stdint.h>
+
+typedef enum {COLOR_GREEN, COLOR_BLUE, COLOR_UNDEFINED} base_color_t;
+
+typedef struct {
+  uint16_t red;
+  uint16_t green;
+  uint16_t blue;
+  uint16_t clear;
+  base_color_t base_color;
+} color_t;
 
 bool colour_sensor_init();
+void update_base_color();
+base_color_t get_base_color();
 
 #endif // ROBOCUP_COLOUR_SENSOR_H
