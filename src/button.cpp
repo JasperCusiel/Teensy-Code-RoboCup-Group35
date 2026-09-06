42c9a6484bcd85fd73edea8c08aa869827633edd
Jasper Cusiel <jaspercusiel@icloud.com>
2026-09-07 10:35:40 +1200

    Tidy and comment

--- src/button.cpp
+++ src/button.cpp
@@ -2,24 +2,28 @@
 // Created by Jasper Cusiel on 03/08/2026.
 //
 #include "button.h"
-#include <arduino.h>
+#include <core_pins.h>
 
 #define DEBOUNCE_TIME_MS 10
 
-bool read_button(uint8_t pin) {
-  static uint32_t lastChange = 0;
-  static bool lastState = HIGH;
+bool read_button(uint8_t pin)
+{
+    // Read and debounce button.
+    static uint32_t lastChange = 0;
+    static bool lastState = HIGH;
 
-  bool current = !digitalRead(pin);
+    bool current = !digitalRead(pin);
 
-  if (current != lastState) {
-    lastChange = millis();
-    lastState = current;
-  }
+    if (current != lastState)
+    {
+        lastChange = millis();
+        lastState = current;
+    }
 
-  if (millis() - lastChange > DEBOUNCE_TIME_MS) {
-    return current;
-  }
+    if (millis() - lastChange > DEBOUNCE_TIME_MS)
+    {
+        return current;
+    }
 
-  return lastState;
+    return lastState;
 }
