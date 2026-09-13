5b723a2a5806e8bdafab5f3707db115cdfe580fc
Jasper Cusiel <jaspercusiel@icloud.com>
2026-09-14 10:41:01 +1200

    Tidy

--- include/ToF-Sensors.h
+++ include/ToF-Sensors.h
@@ -4,10 +4,11 @@
 #include <lidar-config.h>
 #include <stdint.h>
 
-struct lidar_scan {
-  float ranges[NUM_POINTS];
-  float angles[NUM_POINTS];
-  uint8_t sector_index[NUM_POINTS];
+struct lidar_scan
+{
+    float ranges[NUM_POINTS];
+    float angles[NUM_POINTS];
+    uint8_t sector_index[NUM_POINTS];
 };
 
 bool tof_init();
@@ -17,13 +18,11 @@ uint8_t ReadRegister8(uint8_t deviceAddress, uint8_t registerAddress);
 void WriteRegister8(uint8_t deviceAddress, uint8_t registerAddress,
                     uint8_t dataByte);
 bool ResetAndInitializeAllSensors();
-void PlotPolarData(uint8_t SensorNum, uint8_t CurrentZone, uint8_t NumOfZones,
-                   uint16_t Distance);
+void PlotPolarData(uint8_t sensor_num, uint8_t current_zone,
+                   uint16_t r);
 void get_ToFCalibration();
 void get_tof_reading();
-void calculate_sector_indices();
 lidar_scan* get_scan();
-void lidar_init_angles();
 
 
 #endif
--- src/ToF-Sensors.cpp
+++ src/ToF-Sensors.cpp
@@ -2,47 +2,41 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <display.h>
-
 #include <SparkFunSX1509.h>
 
+// ST api libraries are written in C
 extern "C" {
 #include "VL53L1X_api.h"
 #include "VL53L1X_calibration.h"
-  }
+}
+
 #include "stdint.h"
 
+// This module samples a 'scan' from the tof array and derived from ST's 2D lidar example.
 
-// IO expander
+// IO expander controls TOF XSHUT pins
 const byte SX1509_ADDRESS = 0x3F;
-SX1509 io; // Create an SX1509 object to be used throughout
-const uint8_t xshutPins[NumOfTOFSensors] = {5, 4, 3, 2, 1, 0};
+SX1509 io;
+const uint8_t x_shut_pins[NumOfTOFSensors] = {5, 4, 3, 2, 1, 0}; // Ordering of sensors
 
+// Stores scan data
 static lidar_scan scan;
 
-/* ----- VL53L1X variables ----- */
+// ----- VL53L1X variables -----
+constexpr uint16_t Dev_init = 0x29; // Default I2C address is 0x29
 
-uint16_t Dev_init = 0x29; /* I2C address of device 1 */
-float LidarAngle[NumOfTOFSensors*NumOfZonesPerSensor];
-uint16_t LidarDistance[NumOfTOFSensors*NumOfZonesPerSensor];
-uint32_t TimeStamp[NumOfTOFSensors*NumOfZonesPerSensor];
-uint16_t Devs[6] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35};
+float LidarAngle[NumOfTOFSensors * NumOfZonesPerSensor];
+
+uint16_t LidarDistance[NumOfTOFSensors * NumOfZonesPerSensor];
+constexpr uint16_t Devs[6] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35};
 uint16_t Distance;
-uint16_t SignalRate;
-uint16_t SpadNb;
-uint16_t AmbientRate;
-uint16_t SignalPerSpad;
-uint16_t AmbientPerSpad;
 uint8_t RangeStatus;
-uint16_t RangeCounter = 0;
 
-uint16_t zone_center[] = {247, 239, 231, 223, 215, 207, 199, 191, 183,
-                          175, 167, 159, 151, 247, 239, 231, 223, 215};
-// Timing Budget Options:  15, 20, 33, 50, 100, 200, 500
-uint16_t TimingBudget = 15;
-
-uint16_t current_zone = 0;
-char BigBuff[4000];
-char VL53L1X_BUFFER[60]; /* Create a buffer to get data */
+constexpr uint16_t zone_center[NumOfZonesPerSensor] = {239, 207, 175};
+// Long distance mode supports timing budgets: 20, 33, 50, 100, 200, 500.
+constexpr uint16_t TimingBudget = 50;
+constexpr uint32_t InterMeasurementMs = (1000 + RangingFrequencyHz - 1) / RangingFrequencyHz;
+char VL53L1X_BUFFER[60]; // Create a buffer to get data
 
 VL53L1X_ERROR error = 0;
 uint32_t idx = 0;
@@ -51,277 +45,285 @@ uint8_t Zone, Sensor, Timeout;
 uint32_t TimeStart, TimeEnd, TotalTime, CurrentTime;
 uint8_t Sensorcheck;
 
+// Calibration accounts for the fact that each ROI points at different part of sensing cone.
 // #define Calibrate
 #ifdef Calibrate
 int16_t OffsetCal[NumOfTOFSensors * NumOfZonesPerSensor] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
 };
 #else
 int16_t OffsetCal[NumOfTOFSensors * NumOfZonesPerSensor] = {
-    -40, -44, -29, -24, -21, -18, -14, -15, -14, -13, -17, -22, -32, -28, -17,
-    -21, -24, -11, -12, -12, -9,  -11, -26, -25, -36, -38, -36, -46, -26, -24,
-    -20, -16, -17, -14, -11, -18, -17, -26, -37, -43, -31, -21, -25, -20, -8,
-    -12, -12, -13, -20, -27, -26, -38, -51, -41, -36, -24, -22, -21, -19, -16,
-    };
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0,
+};
 
 #endif
 
-bool tof_init() {
-  if (io.begin(SX1509_ADDRESS)) {
-    Serial.println("SX1509 Port Expander Started");
-  }
-
-  for (uint8_t i = 0; i < NumOfTOFSensors; i++) {
-    io.pinMode(xshutPins[i], OUTPUT);
-    io.digitalWrite(xshutPins[i], LOW);
-  }
-  Serial.println("pins driven low");
-  calculate_sector_indices();
-  lidar_init_angles();
-
-  return ResetAndInitializeAllSensors();  // true if all sensors started correctly
-}
-
-void calculate_sector_indices() {
-  for(int j=0;j<NumOfTOFSensors * NumOfZonesPerSensor;j++)
-  {
-    float angle = FOV_MIN + j*(FOV_MAX-FOV_MIN) /((NumOfTOFSensors * NumOfZonesPerSensor) -1);
-
-    int sector = (angle - FOV_MIN)/SECTOR_WIDTH;
-
-    if(sector < 0) {
-      sector=0;
-    }
-
-    if(sector >= NUM_SECTORS) {
-      sector=NUM_SECTORS-1;
-    }
-    scan.sector_index[j] = sector;
-  }
-}
-
-void lidar_init_angles()
+namespace
 {
-  float angle_step =
-      (FOV_MAX - FOV_MIN) /
-      (NUM_POINTS - 1);
-
-
-  for(int i=0;i<NUM_POINTS;i++)
-  {
-    scan.angles[i] =
-        FOV_MIN + i*angle_step;
-  }
-}
-
-void ResetAllSensors() {
-  // Disable/reset all sensors by driving their XSHUT pins low.
-  for (uint8_t i = 0; i < NumOfTOFSensors; i++) {
-    io.pinMode(xshutPins[i], OUTPUT);
-    io.digitalWrite(xshutPins[i], LOW);
-  }
-}
-
-void TurnOnSensor(uint8_t SensorNum) {
-  io.digitalWrite(xshutPins[SensorNum], HIGH);
-  Serial.printf("Starting Sensor %d\n", SensorNum);
-}
-uint8_t dataRead;
-uint8_t ReadRegister8(uint8_t deviceAddress, uint8_t registerAddress) {
-  uint8_t data = 0;
-  uint8_t error = VL53L1_RdByte(deviceAddress, registerAddress, &data);
-  if (error != 0) {
-    Serial.printf("ReadRegister8 failed. Error code: %d\n", error);
-  }
-  return data;
-}
-
-void WriteRegister8(uint8_t deviceAddress, uint8_t registerAddress,
-                    uint8_t dataByte) {
-  uint8_t error = VL53L1_WrByte(deviceAddress, registerAddress, dataByte);
-  if (error != 0) {
-    Serial.printf("WriteRegister8 failed. Error code: %d\n", error);
-  }
-}
-
-bool ResetAndInitializeAllSensors(void) {
-  Serial.println("reset and initalize");
-  uint8_t i, Sensor, error = 0;
-  uint8_t Bootstate = 0;
-  int16_t Offset;
-  ResetAllSensors();
-  delay(10);
-  for (i = 0; i < NumOfTOFSensors; i++) {
-    TurnOnSensor(i);
-    delay(5);
-    error += VL53L1X_BootState(Dev_init, &Bootstate);
-    while (Bootstate != 0x03)
+    // Calculate sector indices used for VFH
+    void calculate_sector_indices()
     {
-      delay(5);
-      error += VL53L1X_BootState(Dev_init, &Bootstate);
+        for (int j = 0; j < NumOfTOFSensors * NumOfZonesPerSensor; j++)
+        {
+            float angle = FOV_MIN + j * (FOV_MAX - FOV_MIN) / ((NumOfTOFSensors * NumOfZonesPerSensor) - 1);
+
+            int sector = (angle - FOV_MIN) / SECTOR_WIDTH;
+
+            if (sector < 0)
+            {
+                sector = 0;
+            }
+
+            if (sector >= NUM_SECTORS)
+            {
+                sector = NUM_SECTORS - 1;
+            }
+            scan.sector_index[j] = sector;
+        }
     }
-    VL53L1X_SensorInit(Dev_init); /* Initialize sensor  */
-    error = VL53L1X_SetI2CAddress(
-        Dev_init,
-        (Devs[i] << 1)); /* Change i2c address Left is now 0x30 and Dev1 left
-                            shift as it uses 7 bit addresses */
-    dataRead = ReadRegister8(Devs[i], static_cast<uint8_t>(0x10f));
-    dataRead = ReadRegister8(Devs[i], static_cast<uint8_t>(0x110));
-  }
-  Serial.print("All Chips booted\n");
 
-  for (Sensor = 0; Sensor < NumOfTOFSensors; Sensor++) {
-    VL53L1X_SetDistanceMode(Devs[Sensor], 1);
-    VL53L1X_SetTimingBudgetInMs(Devs[Sensor], TimingBudget);
-    VL53L1X_SetInterMeasurementInMs(Devs[Sensor], TimingBudget);
-    VL53L1X_SetROI(Devs[Sensor], WidthOfSPADsPerZone, 6);
-    WriteRegister8(Devs[Sensor], ROI_CONFIG__USER_ROI_CENTRE_SPAD,
-                   zone_center[0] - 0);
-    error = VL53L1X_GetOffset(Devs[Sensor], &Offset);
-    VL53L1X_SetOffset(Devs[Sensor], Offset + 40);
-  }
-  for (Sensor = 0; Sensor < NumOfTOFSensors; Sensor++) {
-    VL53L1X_StartRanging(Devs[Sensor]);
-    delay(1);
-  }
-  if (error != 0) {
-    Serial.print("Some Errors seen\n");
-    return false;
-  }
+    // Calculate the angles for each scan
+    void lidar_init_angles()
+    {
+        float angle_step = (FOV_MAX - FOV_MIN) / (NUM_POINTS - 1);
+        for (int i = 0; i < NUM_POINTS; i++)
+        {
+            scan.angles[i] = FOV_MIN + i * angle_step;
+        }
+    }
+}
 
-  return true;
+bool tof_init()
+{
+    // Start the tof array.
+
+    // Turn off all sensors.
+    io.begin(SX1509_ADDRESS);
+    for (uint8_t i = 0; i < NumOfTOFSensors; i++)
+    {
+        io.pinMode(x_shut_pins[i], OUTPUT);
+        io.digitalWrite(x_shut_pins[i], LOW);
+    }
+
+    // Calculate angles and sectors for VFH.
+    calculate_sector_indices();
+    lidar_init_angles();
+
+    return ResetAndInitializeAllSensors(); // True if all sensors started correctly
+}
+
+
+void ResetAllSensors()
+{
+    // Disable/reset all sensors by driving their XSHUT pins low.
+    for (uint8_t i = 0; i < NumOfTOFSensors; i++)
+    {
+        io.pinMode(x_shut_pins[i], OUTPUT);
+        io.digitalWrite(x_shut_pins[i], LOW);
+    }
+}
+
+void TurnOnSensor(uint8_t SensorNum)
+{
+    // Start specific sensor.
+    io.digitalWrite(x_shut_pins[SensorNum], HIGH);
+    Serial.printf("Starting Sensor %d\n", SensorNum);
+}
+
+
+bool ResetAndInitializeAllSensors()
+{
+    uint8_t initialize_error = 0;
+    uint8_t boot_state = 0;
+    int16_t Offset;
+
+    Serial.println("Reset and initialize tof sensors");
+    ResetAllSensors();
+    delay(10); // Let sensors boot
+
+    for (size_t i = 0; i < NumOfTOFSensors; i++)
+    {
+        TurnOnSensor(i);
+        delay(5);
+        initialize_error += VL53L1X_BootState(Dev_init, &boot_state);
+        while (boot_state != 0x03)
+        {
+            delay(5);
+            initialize_error += VL53L1X_BootState(Dev_init, &boot_state);
+        }
+        VL53L1X_SensorInit(Dev_init); // Initialize sensor
+        initialize_error = VL53L1X_SetI2CAddress(Dev_init, (Devs[i] << 1)); // shift as it uses 7 bit addresses
+    }
+    Serial.print("All Chips booted\n");
+    // Apply config
+    for (const unsigned short sensor : Devs)
+    {
+        VL53L1X_SetDistanceMode(sensor, 2);
+        VL53L1X_SetTimingBudgetInMs(sensor, TimingBudget);
+        VL53L1X_SetInterMeasurementInMs(sensor, InterMeasurementMs);
+        VL53L1X_SetROI(sensor, WidthOfSPADsPerZone, 6);
+        VL53L1_WrByte(sensor, ROI_CONFIG__USER_ROI_CENTRE_SPAD, zone_center[0]);
+        initialize_error = VL53L1X_GetOffset(sensor, &Offset);
+        VL53L1X_SetOffset(sensor, (Offset + 40));
+    }
+
+    // Start each sensor
+    for (const unsigned short sensor : Devs)
+    {
+        VL53L1X_StartRanging(sensor);
+        delay(1);
+    }
+
+    if (initialize_error != 0)
+    {
+        Serial.print("Errors seen starting ToF\n");
+        return false;
+    }
+
+    return true;
 }
 
 float OldAngle;
 double SystemAngle;
-void PlotPolarData(uint8_t SensorNum, uint8_t CurrentZone, uint8_t NumOfZones,
-                   uint16_t Distance) {
-  double PartZoneAngle;
 
-  float CorrectedDistance = 0;
+void PlotPolarData(uint8_t sensor_num, uint8_t current_zone, uint16_t r)
+{
+    double corrected_distance = 0;
 
-  if (Distance > 60000) {
-    Distance = 0;
-  }
-  PartZoneAngle = (StartingZoneAngle + ZoneFOVChangePerStep * CurrentZone) -
-                  (HorizontalFOVofSensor / 2.0);
-  SystemAngle = -80 + 20.0 * SensorNum + PartZoneAngle;
-  CorrectedDistance = pow(pow(RadarCircleRadius, 2) + pow(Distance, 2) -
-                              (2 * RadarCircleRadius * Distance *
-                               cos((180 - PartZoneAngle) / (180) * Pi)),
-                          0.5);
-  if (CorrectedDistance < 55) {
-    CorrectedDistance = 55;
-  }
-  LidarAngle[SensorNum * 13 + CurrentZone] = SystemAngle;
-  LidarDistance[SensorNum * 13 + CurrentZone] = (uint16_t)CorrectedDistance;
+    if (r > 60000)
+    {
+        r = 0;
+    }
 
+    double part_zone_angle = (StartingZoneAngle + ZoneFOVChangePerStep * current_zone) - (HorizontalFOVofSensor / 2.0);
+
+    SystemAngle = -80 + 20.0 * sensor_num + part_zone_angle;
+    corrected_distance = pow(
+        pow(RadarCircleRadius, 2) + pow(r, 2) - (2 * RadarCircleRadius * r * cos(
+            (180 - part_zone_angle) / (180) * Pi)), 0.5);
+
+    // We can't be less than mounting radius
+    if (corrected_distance < RadarCircleRadius)
+    {
+        corrected_distance = RadarCircleRadius;
+    }
+
+    LidarAngle[sensor_num * NumOfZonesPerSensor + current_zone] = static_cast<float>(SystemAngle);
+    LidarDistance[sensor_num * NumOfZonesPerSensor + current_zone] = static_cast<uint16_t>(corrected_distance);
 }
 
-void get_ToFCalibration() {
-  for (uint8_t Sensor = 0; Sensor < NumOfTOFSensors; Sensor++) {
-    for (uint8_t Zone = 0; Zone < NumOfZonesPerSensor; Zone++) {
-      WriteRegister8(Devs[Sensor], ROI_CONFIG__USER_ROI_CENTRE_SPAD, zone_center[Zone + 1]);
-      delay(5);
-      int16_t offset = 0;
-      VL53L1X_CalibrateOffset(Devs[Sensor], 300, &offset);
+void get_ToFCalibration()
+{
+    for (uint8_t Sensor = 0; Sensor < NumOfTOFSensors; Sensor++)
+    {
+        for (uint8_t Zone = 0; Zone < NumOfZonesPerSensor; Zone++)
+        {
+            VL53L1_WrByte(Devs[Sensor], ROI_CONFIG__USER_ROI_CENTRE_SPAD, zone_center[Zone]);
+            delay(5);
+            int16_t offset = 0;
+            VL53L1X_CalibrateOffset(Devs[Sensor], 300, &offset);
 
-      Serial.print(offset);
-      if (Zone < NumOfZonesPerSensor - 1) {
-        Serial.print(",");
-      }
-    }
-    Serial.println();  // newline after each sensor's 13 values
-  }
-}
-
-
-void get_tof_reading() {
-  error = 0;
-  TimeStart = millis();
-  Timeout = 0;
-  for (Zone = 0; Zone < NumOfZonesPerSensor; Zone++) {
-    for (Sensor = 0; Sensor < NumOfTOFSensors; Sensor++) {
-      WriteRegister8(Devs[Sensor], ROI_CONFIG__USER_ROI_CENTRE_SPAD,
-                     zone_center[Zone + 1] - 0);
-    }
-    idx = idx + 1;
-    for (Sensor = 0; Sensor < NumOfTOFSensors; Sensor++) {
-      error = VL53L1X_CheckForDataReady(Devs[Sensor], &Sensorcheck);
-      while ((Sensorcheck == 0) && (Timeout == 0)) {
-        // delay(1);
-        CurrentTime = millis();
-        if (CurrentTime >
-            (TimeStart + (NumOfZonesPerSensor + 1) * TimingBudget * 2)) {
-          Timeout = 1;
-          Sensor = NumOfTOFSensors;
-          Zone = NumOfZonesPerSensor;
-        } else {
-          error += VL53L1X_CheckForDataReady(Devs[Sensor], &Sensorcheck);
-        }
-      }
-      if (Timeout == 0) {
-        WriteRegister8(Devs[Sensor], ROI_CONFIG__USER_ROI_CENTRE_SPAD,
-                       zone_center[Zone + 1] - 0);
-        TimeStamp[Sensor * 13 + Zone] = millis();
-        VL53L1X_ClearInterrupt(Devs[Sensor]);
-
-        error += VL53L1X_GetDistance(Devs[Sensor], &Distance);
-        error += VL53L1X_GetRangeStatus(Devs[Sensor], &RangeStatus);
-        if ((RangeStatus == 0) || (RangeStatus == 7)) {
-          if (Distance > 60000) {
-            Distance = 0;
-            PlotPolarData(Sensor, Zone, 13, 0);
-          } else {
-            Distance = Distance + OffsetCal[Sensor * 13 + Zone];
-            if (Distance > 60000) {
-              Distance = 0;
+            Serial.print(offset);
+            if (Zone < NumOfZonesPerSensor - 1)
+            {
+                Serial.print(",");
             }
-            PlotPolarData(Sensor, Zone, 13, Distance);
-          }
-        } else {
-          PlotPolarData(Sensor, Zone, 13, 4000);
         }
-      }
+        Serial.println(); // newline after each sensor's ROI values
     }
-  }
-  if (Timeout == 1) {
-    ResetAndInitializeAllSensors();
-    Timeout = 0;
-    Serial.print("Reset Performed\n");
-  } else {
-    // delay(TimingBudget);
-    // TimeEnd = millis();
-    // TotalTime = (TimeEnd - TimeStart);
-    // snprintf(BigBuff, sizeof(BigBuff), "Time: %ld\n", TotalTime);
-    // Serial.print(BigBuff);
-
-    #ifdef DEBUG
-    // Distances
-    Serial.print("D ");
-    for (int i = 0; i < 78; i++)
-      Serial.printf("%u ", LidarDistance[i]);
-    Serial.print("\n");
-
-    // Angles
-    Serial.print("A ");
-    for (int i = 0; i < 78; i++)
-      Serial.printf("%.1f ", LidarAngle[i]);
-    Serial.print("\n");
-    #endif
-    for (size_t n = 0; n < NumOfTOFSensors * NumOfZonesPerSensor; n++) {
-      scan.ranges[n] = LidarDistance[n] / 1000.0f;
-    }
-
-  }
-  if (error != 0) {
-    Serial.print("Some Errors seen\n");
-  }
 }
 
-lidar_scan* get_scan() {
-  return &scan;
+
+void get_tof_reading()
+{
+    error = 0;
+    TimeStart = millis();
+    Timeout = 0;
+    for (Zone = 0; Zone < NumOfZonesPerSensor; Zone++)
+    {
+        for (Sensor = 0; Sensor < NumOfTOFSensors; Sensor++)
+        {
+            VL53L1_WrByte(Devs[Sensor], ROI_CONFIG__USER_ROI_CENTRE_SPAD, zone_center[Zone]);
+        }
+        idx = idx + 1;
+        for (Sensor = 0; Sensor < NumOfTOFSensors; Sensor++)
+        {
+            error = VL53L1X_CheckForDataReady(Devs[Sensor], &Sensorcheck);
+            while ((Sensorcheck == 0) && (Timeout == 0))
+            {
+                CurrentTime = millis();
+                if (CurrentTime > (TimeStart + (NumOfZonesPerSensor + 1) * TimingBudget * 2))
+                {
+                    Timeout = 1;
+                    Sensor = NumOfTOFSensors;
+                    Zone = NumOfZonesPerSensor;
+                }
+                else
+                {
+                    error += VL53L1X_CheckForDataReady(Devs[Sensor], &Sensorcheck);
+                }
+            }
+            if (Timeout == 0)
+            {
+                VL53L1_WrByte(Devs[Sensor], ROI_CONFIG__USER_ROI_CENTRE_SPAD, zone_center[Zone]);
+                VL53L1X_ClearInterrupt(Devs[Sensor]);
+
+                error += VL53L1X_GetDistance(Devs[Sensor], &Distance);
+                error += VL53L1X_GetRangeStatus(Devs[Sensor], &RangeStatus);
+                if ((RangeStatus == 0) || (RangeStatus == 7))
+                {
+                    if (Distance > 60000)
+                    {
+                        Distance = 0;
+                        PlotPolarData(Sensor, Zone, 0);
+                    }
+                    else
+                    {
+                        Distance = Distance + OffsetCal[Sensor * NumOfZonesPerSensor + Zone];
+                        if (Distance > 60000)
+                        {
+                            Distance = 0;
+                        }
+                        PlotPolarData(Sensor, Zone, Distance);
+                    }
+                }
+                else
+                {
+                    PlotPolarData(Sensor, Zone, 4000);
+                }
+            }
+        }
+    }
+    if (Timeout == 1)
+    {
+        ResetAndInitializeAllSensors();
+        Timeout = 0;
+        Serial.print("Reset Performed\n");
+    }
+    else
+    {
+        for (size_t n = 0; n < NumOfTOFSensors * NumOfZonesPerSensor; n++)
+        {
+            scan.ranges[n] = LidarDistance[n] / 1000.0f;
+        }
+    }
+    if (error != 0)
+    {
+        Serial.print("Some Errors seen\n");
+    }
+}
+
+lidar_scan* get_scan()
+{
+    return &scan;
 }
